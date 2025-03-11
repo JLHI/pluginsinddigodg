5fb3c3038892e1864a942bc091bdb4d28fbba2dc
JLHI <jl.humbert@inddigo.com>
2025-03-11 16:17:46 +0100

    rollback

--- Itineraire_ign/ItineraireParLaRoute_algorithm.py
+++ Itineraire_ign/ItineraireParLaRoute_algorithm.py
@@ -151,8 +151,7 @@ class ItineraireParLaRouteAlgorithm(QgsProcessingAlgorithm):
         crs_projected = QgsCoordinateReferenceSystem("EPSG:2154")  # Lambert 93
         crs_wgs84 = QgsCoordinateReferenceSystem("EPSG:4326")  # WGS 84
         transform_to_projected = QgsCoordinateTransform(source1.sourceCrs(), crs_projected, context.transformContext())
-        transform_to_projected2 = QgsCoordinateTransform(source2.sourceCrs(), crs_projected, context.transformContext())
-
+        
         transform_to_wgs84 = QgsCoordinateTransform(crs_projected, crs_wgs84, context.transformContext())
 
         # Transformer les entités dans un système de coordonnées projeté
@@ -160,7 +159,7 @@ class ItineraireParLaRouteAlgorithm(QgsProcessingAlgorithm):
             self.transformFeature(feature, transform_to_projected) for feature in source1.getFeatures()
         ]
         features2 = [
-            self.transformFeature(feature, transform_to_projected2) for feature in source2.getFeatures()
+            self.transformFeature(feature, transform_to_projected) for feature in source2.getFeatures()
         ]
 
         # Filtrer par champs communs si spécifiés
@@ -226,8 +225,8 @@ class ItineraireParLaRouteAlgorithm(QgsProcessingAlgorithm):
                 time.sleep(0.222)
                 id2 = feature2[id_field2]
                 # Transformer les coordonnées pour l'API
-                point1 = transform_to_projected.transform(feature1.geometry().asPoint())
-                point2 = transform_to_projected2.transform(feature2.geometry().asPoint())
+                point1 = transform_to_wgs84.transform(feature1.geometry().asPoint())
+                point2 = transform_to_wgs84.transform(feature2.geometry().asPoint())
 
                 url = QUrl(f"https://data.geopf.fr/navigation/itineraire?resource=bdtopo-osrm&profile={mode}&start={point1.x()},{point1.y()}&end={point2.x()},{point2.y()}")
                 request = QNetworkRequest(url)

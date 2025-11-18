30ab5e1ddc7e33b4919161dfea11ccd15853915c
JLHI <jl.humbert@inddigo.com>
2025-11-18 09:08:56 +0100

    add xlsxwriter lib

--- lib/xlsxwriter/__init__.py
+++ lib/xlsxwriter/__init__.py
@@ -3,8 +3,12 @@
 #
 # Copyright (c) 2013-2025, John McNamara, jmcnamara@cpan.org
 #
-__version__ = "3.2.9"
-__VERSION__ = __version__
-from .workbook import Workbook  # noqa
+import sys
+import os
 
-__all__ = ["Workbook"]
+plugin_dir = os.path.dirname(__file__)
+lib_dir = os.path.join(plugin_dir, "lib")
+
+# Ajout au PYTHONPATH
+if lib_dir not in sys.path:
+    sys.path.insert(0, lib_dir)
